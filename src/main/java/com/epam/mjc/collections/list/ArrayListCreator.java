b6ebdfc5acd02d62730180c3bcc55fa76fb9f902
Sherzod1610 <sherzodrasuljonov086@gmail.com>
2023-05-17 00:31:44 +0500

    finished

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -7,11 +7,11 @@ public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
         ArrayList<String> list = new ArrayList<>(sourceList);
         ArrayList<String> list1 = new ArrayList<>();
-        if (list.isEmpty()) {
+        if (list.size() == 0) {
             return list1;
         }
-        for (int i = 0; i <= list.size(); i++) {
-            if ((i + 1) % 3 == 0) {
+        for (int i = 1; i <= list.size(); i++) {
+            if ((i) % 3 == 0) {
                 list1.add(list1.get(i));
                 list1.add(list1.get(i));
             }

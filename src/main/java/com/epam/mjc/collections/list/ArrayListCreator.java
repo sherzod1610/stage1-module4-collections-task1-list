43d75f814a7bc5f6a13ac42bb700abfec8c284ef
Sherzod1610 <sherzodrasuljonov086@gmail.com>
2023-05-16 18:27:20 +0500

    finished

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> list = new ArrayList<>(sourceList);
+        ArrayList<String> list1 = new ArrayList<>();
+        for (int i = 0; i <= list.size(); i++) {
+            if ((i + 1) % 3 == 0) {
+                list1.add(list1.get(i));
+                list1.add(list1.get(i));
+            }
+        }
+        return list1;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        List<Integer> list = new LinkedList<>(sourceList);
+        LinkedList<Integer> list1 = new LinkedList<>();
+        for (Integer integer : list) {
+            if (integer % 2 == 0) {
+                list1.addLast(integer);
+            } else {
+                list1.addFirst(integer);
+            }
+        }
+        return list1;
     }
 }
